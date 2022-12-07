5e188a5dd7a7f63f7d52f3ab28391dd71f4f83e8
Roman Sitko <sitkoroman@gmail.com>
2022-12-07 16:11:08 +0200

    renew main method in Practical task 2

--- src/com/softserve/edu04/MyProjects/PracticalTask2/Main.java
+++ src/com/softserve/edu04/MyProjects/PracticalTask2/Main.java
@@ -1,6 +1,6 @@
 package com.softserve.edu04.MyProjects.PracticalTask2;
 
-/*Practical task. Enter the number of the day of the week. Display the name in two languages.
+/*Practical task 2. Enter the number of the day of the week. Display the name in two languages.
 Roman Sitko */
 
 import java.io.BufferedReader;
@@ -11,35 +11,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter number by word and small letters");
-        String word = br.readLine();
+        System.out.println("Enter number of day from 1 to 7");
+        int number = Integer.parseInt(br.readLine());
 
-        switch (word) {
-            case "one":
+        switch (number) {
+            case 1:
                 Day day1 = Day.ONE;
                 System.out.println(day1.getEn() + ", " + day1.getUa());
                 break;
-            case "two":
+            case 2:
                 Day day2 = Day.TWO;
                 System.out.println(day2.getEn() + ", " + day2.getUa());
                 break;
-            case "three":
+            case 3:
                 Day day3 = Day.THREE;
                 System.out.println(day3.getEn() + ", " + day3.getUa());
                 break;
-            case "four":
+            case 4:
                 Day day4 = Day.FOUR;
                 System.out.println(day4.getEn() + ", " + day4.getUa());
                 break;
-            case "five":
+            case 5:
                 Day day5 = Day.FIVE;
                 System.out.println(day5.getEn() + ", " + day5.getUa());
                 break;
-            case "six":
+            case 6:
                 Day day6 = Day.SIX;
                 System.out.println(day6.getEn() + ", " + day6.getUa());
                 break;
-            case "seven":
+            case 7:
                 Day day7 = Day.SEVEN;
                 System.out.println(day7.getEn() + ", " + day7.getUa());
                 break;
