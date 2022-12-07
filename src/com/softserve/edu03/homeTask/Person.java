11a60c5f78c12294e53c2496405562c43a719582
Anisimov_Eugene <zhecky2004@gmail.com>
2022-12-07 14:24:08 +0200

    +HomeTask

---
+++ src/com/softserve/edu03/homeTask/Person.java
@@ -0,0 +1,78 @@
+package com.softserve.edu03.homeTask;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge() {
+        return LocalDate.now().getYear() - birthYear;
+    }
+
+    public void input() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input birth year");
+        birthYear = scan.nextInt();
+    }
+
+
+    public String output() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
+
+    public void changeName(String fn, String ln) {
+        firstName = !fn.isBlank() ? fn : firstName;
+        lastName = !ln.isBlank() ? ln : lastName;
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Mike", "Tyson");
+        person1.input();
+        System.out.println(person1.getAge() + " years");
+        System.out.println(person1.output());
+        person1.changeName("Tyson", "Fury");
+        System.out.println(person1.output());
+        person1.changeName("", "  ");
+        System.out.println(person1.output());
+
+    }
+}
--- src/com/softserve/edu03/practicalTask/Main.java
+++ src/com/softserve/edu03/practicalTask/Main.java
@@ -3,6 +3,7 @@ package com.softserve.edu03.practicalTask;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Employee Dan = new Employee("Dan", 5, 20);
         Employee Mike = new Employee("Mike", 7, 10);
