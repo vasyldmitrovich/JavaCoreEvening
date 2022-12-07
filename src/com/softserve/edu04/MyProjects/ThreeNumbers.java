ac9b99043c5352b715d3edf0b56bb9b9037485e6
Roman Sitko <sitkoroman@gmail.com>
2022-12-07 13:18:29 +0200

    Practical task. class With main method Enter three numbers. Find out how man of them are odd.

--- src/com/softserve/edu04/MyProjects/ThreeNumbers.java
+++ src/com/softserve/edu04/MyProjects/ThreeNumbers.java
@@ -4,17 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*Practical task. Enter three numbers. Find out how many of them are odd.
+ Roman Sitko. With main method*/
 public class ThreeNumbers {
+    private static int countOdd = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter three numbers");
+        System.out.println("Enter three any integer numbers");
         int num1 = Integer.parseInt(br.readLine());
         int num2 = Integer.parseInt(br.readLine());
         int num3 = Integer.parseInt(br.readLine());
 
-        if (num1 % 2 == 0) {}
+        if (num1 % 2 == 0) {
+            countOdd += 1;
+        }
+        if (num2 % 2 == 0) {
+            countOdd += 1;
+        }
+        if (num3 % 2 == 0) {
+            countOdd += 1;
+        }
 
-        System.out.println(num1 + num2 + num3);
+        System.out.println("Count odd: " + countOdd);
 
     }
 }
