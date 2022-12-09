4895450fb0855f91d08a98e8314f03f1e99e9221
Anisimov_Eugene <zhecky2004@gmail.com>
2022-12-09 17:04:23 +0200

    hw

---
+++ src/com/softserve/edu05/practicalTask/Main.java
@@ -0,0 +1,10 @@
+package com.softserve.edu05.practicalTask;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        firstTask.arrayFunctions();
+
+    }
+}
---
+++ src/com/softserve/edu05/practicalTask/firstTask.java
@@ -0,0 +1,37 @@
+package com.softserve.edu05.practicalTask;
+
+import java.util.Random;
+
+public class firstTask {
+    static Random random = new Random();
+
+    public static void arrayFunctions() {
+        int sumPositive = 0;
+        int counterNEgative = 0;
+        int max = Integer.MIN_VALUE;
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -10 + random.nextInt(21);
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+
+        for (int num : arr) {
+            max = num > max ? num : max;
+        }
+
+        for (int num : arr) {
+            counterNEgative = num < 0 ? ++counterNEgative : counterNEgative;
+        }
+
+        for (int num : arr) {
+            sumPositive = num > 0 ? sumPositive += num : sumPositive;
+        }
+        System.out.println("Max value - " + max + ", sum of positive numbers - " + sumPositive + ", amount of negative numbers - " + counterNEgative);
+
+    }
+}
