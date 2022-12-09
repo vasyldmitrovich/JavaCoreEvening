8eeab3d8a525f564904a1a2979736a82a77dceed
Roman Sitko <sitkoroman@gmail.com>
2022-12-09 17:36:54 +0200

    Practical tasks 4

--- src/com/softserve/edu04/MyProjects/PracticalTask4/Main.java
+++ src/com/softserve/edu04/MyProjects/PracticalTask4/Main.java
@@ -2,10 +2,11 @@ package com.softserve.edu04.MyProjects.PracticalTask4;
 
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product("bred", 25, 3);
+        Product product1 = new Product("bred", 69, 3);
         Product product2 = new Product("butter", 70, 3);
         Product product3 = new Product("milk", 40, 3);
 
+        product1.mostExpensive(product2);
     }
 
 }
