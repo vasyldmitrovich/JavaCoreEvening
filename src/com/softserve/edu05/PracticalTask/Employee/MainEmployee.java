8fb037b31c2f250e2c7bcd2e9cca4ff4e0c4d37f
YuliaPylypenko <juljulip20@gmail.com>
2022-12-10 22:48:32 +0200

    add PracticalTask EDU5

--- src/com/softserve/edu05/PracticalTask/Employee/MainEmployee.java
+++ src/com/softserve/edu05/PracticalTask/Employee/MainEmployee.java
@@ -3,16 +3,19 @@ package com.softserve.edu05.PracticalTask.Employee;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class MainEmployee {
 
     public static void main(String[] args) throws IOException {
-        Employee [] employees = {
+        Employee[] employees;
+        employees = new Employee[]{
                 new Employee("Yaro", 3600, 101),
                 new Employee("Katy", 1111, 101),
                 new Employee("Mary", 1200, 200),
                 new Employee("John", 2255, 200),
-                new Employee("John", 2255, 200),
+                new Employee("John", 2255, 300),
                 new Employee("Mark", 2255, 300),
         };
 
@@ -20,16 +23,20 @@ public class MainEmployee {
         System.out.println("Enter department number:");
 
         int number = Integer.parseInt(br.readLine());
-        if (number!=101 && number!=200 && number!=300) {
+        if (number != 101 && number != 200 && number != 300) {
             System.out.println("There is no such department, please, try another one.");
         }
         Employee search = null;
         for (Employee empl : employees) {
             if (empl.getDepartment_number() == number) {
                 search = empl;
-            }
-            else continue;
+            } else continue;
             System.out.println("The workers of the department: " + search);
         }
+
+        System.out.println("\nEmployees were sorted by salary: ");
+        Arrays.sort(employees, Comparator.comparing(Employee::getSalary));
+        String elements = Arrays.toString(employees);
+        System.out.println(elements);
     }
 }
