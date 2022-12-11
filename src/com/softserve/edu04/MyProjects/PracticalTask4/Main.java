13909019074733663329b6aef335e33a887cbdef
Roman Sitko <sitkoroman@gmail.com>
2022-12-12 00:00:17 +0200

    L4 practical task 4. Working

--- src/com/softserve/edu04/MyProjects/PracticalTask4/Main.java
+++ src/com/softserve/edu04/MyProjects/PracticalTask4/Main.java
@@ -2,11 +2,16 @@ package com.softserve.edu04.MyProjects.PracticalTask4;
 
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product("bred", 69, 3);
-        Product product2 = new Product("butter", 70, 3);
-        Product product3 = new Product("milk", 40, 3);
+        Product[] product = new Product[3];
+//                = new Product("bred", 69, 3);
+//        Product product2 = new Product("butter", 70, 3);
+//        Product product3 = new Product("milk", 40, 3);
 
-        product1.mostExpensive(product2);
+        product[0] = new Product("bred", 69, 3);
+        product[1] = new Product("butter", 70, 3);
+        product[2] = new Product("milk", 72, 3);
+
+        Product.mostExpensive(product);
     }
 
 }
--- src/com/softserve/edu04/MyProjects/PracticalTask4/Product.java
+++ src/com/softserve/edu04/MyProjects/PracticalTask4/Product.java
@@ -11,9 +11,37 @@ public class Product {
         this.quantity = quantity;
     }
 
-    public void mostExpensive (Product p) {
-        if(p.price > price)
-            System.out.println("abc");
+    public static void mostExpensive (Product[] p) {
+        int a = 0;
+//        for(Product i :  p) {
+//            if(i.price >= i.price) {
+//                System.out.println(i.price);
+//            }
+//        }
+        System.out.println(p.length);
+        for (int j = 0; j < p.length; j++) {
+            if (p[j].price > p[j+1].price) {
+                a = p[j].price;
+            }
+        }
+        System.out.println(a);
+
+
+//        int[] a = {7, 2, 8, 9, 0, 10};
+//        int  b = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            if(a[i] > a[i + 1]) {
+//                b = a[i +1];
+//                a[i + 1] = a[i];
+//                a[i] = b;
+//                b = a[i +1];
+//            }
+////            else {
+////                b = a[i + 1];
+////            }
+//        }
+//        System.out.println(b);
+
     }
 
 }
