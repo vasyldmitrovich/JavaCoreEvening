b99d702fb03461352bd7d7ca175f7750f19e6840
Oleg Lastochkin <oleglastochkin@gmail.com>
2022-12-13 20:20:21 +0200

    add Practical Task

---
+++ src/com/softserve/edu05/PracticalTasks/Employee.java
@@ -0,0 +1,86 @@
+package softserve.edu05.PracticalTasks;
+
+import java.sql.Array;
+import java.util.Scanner;
+
+public class Employee {
+    String name;
+    int departmentNumber;
+    double salary;
+
+    public Employee(String name, int departmentNumber, double salary) {
+        this.name = name;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", departmentNumber=" + departmentNumber +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Alpha", 1, 1000);
+        Employee employee2 = new Employee("Beta", 2, 2000);
+        Employee employee3 = new Employee("Gamma", 3, 3000);
+        Employee employee4 = new Employee("Delta", 4, 4000);
+        Employee employee5 = new Employee("Epsilon", 5, 5000);
+
+        Employee[] employees = {employee1, employee2, employee3, employee4, employee5};
+        System.out.println("Input department number");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("Employees of a certain department:");
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getDepartmentNumber() == number){
+                System.out.println(employees[i]);
+            }
+        }
+
+        System.out.println();
+
+        Employee tmp;
+
+        for (int i = 0; i < employees.length-1; i++) {
+            for (int j = i+1; j < employees.length ; j++) {
+                if (employees[i].getSalary()<employees[j].getSalary()) {
+                    tmp = employees[i];
+                    employees[i]=employees[j];
+                    employees[j] = tmp;
+                }
+            }
+        }
+
+        for (Employee emp: employees){
+            System.out.println(emp);
+        }
+    }
+}
---
+++ src/com/softserve/edu05/PracticalTasks/Tasks.java
@@ -0,0 +1,41 @@
+package softserve.edu05.PracticalTasks;
+
+public class Tasks {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//        the biggest of these numbers
+
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max){
+                max = array[i];
+                index = i;
+            }
+        }
+        System.out.println("The biggest number of array[" + index + "]: " + max);
+
+
+//        the sum of positive numbers in the array
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0){
+                sum = sum + array[i];
+            }
+        }
+        System.out.println("Sum of positive numbers: " + sum);
+
+//        the amount of negative numbers in the array
+
+        int amount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0){
+                amount++;
+            }
+        }
+        System.out.println("The amount of negative numbers: " + amount);
+
+    }
+}
