f16bd61fd13473dcee0cda96b6799f20155a16ae
Kirill Lutsenko <kirill.lutsenko1991@gmail.com>
2022-12-21 09:13:45 +0200

    Added 4th task of 1st hometask

---
+++ src/com/softserve/edu03/phoneCall.java
@@ -0,0 +1,44 @@
+package softserve.edu03;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class phoneCall {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Tell me how much first call costs per minute:");
+		double c1 = scanner.nextDouble();
+		System.out.println("Now tell me how long your phone call was in seconds:");
+		double t1 = scanner.nextDouble()/60;
+		System.out.println("Tell me how much second call costs per minute:");
+		double c2 = scanner.nextDouble();
+		System.out.println("Now tell me how long your phone call was in seconds:");
+		double t2 = scanner.nextDouble()/60;
+		System.out.println("Tell me how much third call costs per minute:");
+		double c3 = scanner.nextDouble();
+		System.out.println("Now tell me how long your phone call was in seconds:");
+		double t3 = scanner.nextDouble()/60;
+		scanner.close();
+		
+		double s1 = multiply(c1,t1);
+		System.out.println(s1);
+		double s2 = multiply(c2,t2);
+		System.out.println(s1);
+		double s3 = multiply(c3,t3);
+		System.out.println(s3);
+		
+		double totalSum = s1+s2+s3;
+		DecimalFormat decimalFormat = new DecimalFormat("#.##");
+		String result = decimalFormat.format(totalSum);
+		
+		System.out.println("The sum of 3 calls is:" +totalSum);     //  не вийшло округлити до #,##
+	}
+	
+	static double multiply(double c, double t) {
+		return c*t;
+	}
+
+}
