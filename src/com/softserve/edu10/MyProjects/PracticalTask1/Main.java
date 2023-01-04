719c94a381ce75edd2980f929d9c505feb870dba
Roman Sitko <sitkoroman@gmail.com>
2023-01-04 17:45:09 +0200

    Upgrade. Lesson 9 Homework

---
+++ src/com/softserve/edu10/MyProjects/PracticalTask1/Main.java
@@ -0,0 +1,85 @@
+package com.softserve.edu10.MyProjects.PracticalTask1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Map<Integer, String> employeeMap = new HashMap<>();
+
+        employeeMap.put(0, "Bob Sting");
+        employeeMap.put(1, "Bill Still");
+        employeeMap.put(2, "Vasyl Zubriv");
+        employeeMap.put(3, "Mark Prokopenko");
+        employeeMap.put(4, "Max Zayets");
+        employeeMap.put(5, "Borys Santa");
+        employeeMap.put(6, "Dmytro Shylyak");
+        employeeMap.put(7, "Dmytro Shylyak");
+
+
+        System.out.println(employeeMap);
+        for(Map.Entry<Integer, String> entry : employeeMap.entrySet()) {
+            if (employeeMap.containsValue("Dmytro Shylyak")) {
+                System.out.println(employeeMap.get(entry.getKey()));
+//                System.out.println("rrr");
+            }
+        }
+
+//        for(Map.Entry<Integer, String> entry : employeeMap.entrySet()) {
+//            if(entry.getValue().equals("Dmytro Shylyak")) {
+//                System.out.println("Key = " + entry.getKey());
+//            }
+//        }
+
+
+
+/*
+
+        for(Map.Entry<Integer, String> entry : employeeMap.entrySet()) {
+            System.out.println("Key = " + entry.getKey() + ", value = " + entry.getValue());
+        }
+*/
+
+
+/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int key = -1;
+        do {
+            System.out.print("Enter key: ");
+            key = Integer.parseInt(br.readLine());
+
+            if(employeeMap.containsKey(key)) {
+                System.out.print(employeeMap.get(key) + "\n");
+            } else {
+                System.out.print("Wrong key\n");
+            }
+        } while (employeeMap.containsKey(key));
+*/
+
+
+
+/*        String value = "";
+        System.out.print("Enter value: ");
+        value = br.readLine();
+
+        for(Map.Entry<Integer, String> entry : employeeMap.entrySet()) {
+            if(entry.getValue().contains(value)) {
+                System.out.println("Key = " + entry.getKey());
+                break;
+            }
+        }*/
+
+
+
+
+//        for(Map.Entry<Integer, String> entry : employeeMap.entrySet()) {
+//            if(entry.getKey().equals(key)) {
+//                System.out.println();
+//            }
+//        }
+
+    }
+}
