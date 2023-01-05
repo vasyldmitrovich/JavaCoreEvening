4e9337ddf641341ae35a577acc843069a13d22cd
Anisimov_Eugene <zhecky2004@gmail.com>
2023-01-05 14:06:25 +0200

    +homeTask and practicalTask

--- src/com/softserve/edu11/practicalTask/Main.java
+++ src/com/softserve/edu11/practicalTask/Main.java
@@ -7,12 +7,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+
+
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         runFirst();
         runSecond();
         runThird();
+        String str = "asd;";
+
     }
 
     static void runFirst() {
