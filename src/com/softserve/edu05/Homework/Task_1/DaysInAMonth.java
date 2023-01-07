0f108800fa4ba3a74b189a41f11e87a88533f998
Nazar Petryshyn <petryshynazary@gmail.com>
2023-01-07 13:55:49 +0200

    reworked method.

--- src/com/softserve/edu05/Homework/Task_1/DaysInAMonth.java
+++ src/com/softserve/edu05/Homework/Task_1/DaysInAMonth.java
@@ -3,19 +3,32 @@ package com.softserve.edu05.Homework.Task_1;
 import java.util.Scanner;
 
 public class DaysInAMonth {
-    public String daysInAMonth() {
-        int[] daysInAMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31 ,30 , 31};
+
+    public int inPut() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter month number.");
         int a = scanner.nextInt();
         scanner.close();
+        return a;
+    }
+
+    public int whichMonth(int a) {
+        int[] whichMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31 ,30 , 31};
         if (a < 0 | a > 12) {
-            return  "Incorrect month number.";
+            return  -1;
         } else {
-            return  "Month number " + a + " contains " + (daysInAMonths[a - 1]) + " days.";
+            return  whichMonth[a - 1];
         }
     }
 
+    public String daysInAMonth(){
+        int a = whichMonth(inPut());
+        if (a != -1) {
+            return "Selected month contains " + a + " days.";
+        }else {
+            return "Incorrect month number.";
+        }
+    }
     public static void main(String[] args) {
         DaysInAMonth daysInAMonth = new DaysInAMonth();
         System.out.println(daysInAMonth.daysInAMonth());
