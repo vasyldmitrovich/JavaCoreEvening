ad107531b77a85f88b3b9a3975eb08413f67a72a
Roman Sitko <sitkoroman@gmail.com>
2023-01-11 12:55:26 +0200

    add Vasyl's examples

---
+++ src/com/softserve/edu12/working_dialog/ApplTh.java
@@ -0,0 +1,9 @@
+package com.softserve.edu12.working_dialog;
+
+public class ApplTh {
+	public static void main(String[] args) {
+		DialogTime dt = new DialogTime();
+		dt.setVisible(true);
+		System.out.println("Appl: Thread ID = " + Thread.currentThread().getId());
+	}
+}
