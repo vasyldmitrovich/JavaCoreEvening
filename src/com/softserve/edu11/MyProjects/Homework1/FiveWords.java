4ff344893092e51badbd7cc6c6ca62267ba7afed
Roman Sitko <sitkoroman@gmail.com>
2023-01-16 23:36:51 +0200

    Working. Lesson 11 Homework 1

---
+++ src/com/softserve/edu11/MyProjects/Homework1/FiveWords.java
@@ -0,0 +1,28 @@
+package com.softserve.edu11.MyProjects.Homework1;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FiveWords {
+
+    public static void biggestWord(String text) {
+        String pattern = "[a-zA-Z]+";
+
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(text);
+
+        // 2. Get all parts
+        m.reset(); // Reset Iterator
+
+        Map<Integer, String> map = new HashMap<>();
+        while (m.find()) {
+//            System.out.println(" *" + text.substring(m.start(), m.end()).length() + "* ");
+            map.put(text.substring(m.start(), m.end()).length(), text.substring(m.start(), m.end()));
+        }
+        int maxKey = Collections.max(map.keySet());
+        System.out.println(map.get(maxKey));
+    }
+}
---
+++ src/com/softserve/edu11/MyProjects/Homework1/Main.java
@@ -0,0 +1,13 @@
+package com.softserve.edu11.MyProjects.Homework1;
+
+/*Roman Sitko
+* Lesson 11 Homework 1
+* */
+public class Main {
+    public static void main(String[] args) {
+        FiveWords.biggestWord("My name is Roman");
+        FiveWords.biggestWord("His work is hard");
+        FiveWords.biggestWord("This watch is most expensive in the world");
+        FiveWords.biggestWord("He has a yellow car");
+    }
+}
