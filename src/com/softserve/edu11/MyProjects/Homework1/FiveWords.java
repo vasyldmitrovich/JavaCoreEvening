b69883b74ae2422ea6de94983301789f777ef5a1
Roman Sitko <sitkoroman@gmail.com>
2023-01-17 17:41:36 +0200

    upgrade. Lesson 11, Homework 1

--- src/com/softserve/edu11/MyProjects/Homework1/FiveWords.java
+++ src/com/softserve/edu11/MyProjects/Homework1/FiveWords.java
@@ -1,8 +1,6 @@
 package com.softserve.edu11.MyProjects.Homework1;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,12 +15,22 @@ public class FiveWords {
         // 2. Get all parts
         m.reset(); // Reset Iterator
 
-        Map<Integer, String> map = new HashMap<>();
+//        Map<Integer, String> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+
         while (m.find()) {
 //            System.out.println(" *" + text.substring(m.start(), m.end()).length() + "* ");
-            map.put(text.substring(m.start(), m.end()).length(), text.substring(m.start(), m.end()));
+//            map.put(text.substring(m.start(), m.end()).length(), text.substring(m.start(), m.end()));
+            list.add(text.substring(m.start(), m.end()));
+            list1.add(text.substring(m.start(), m.end()).length());
+
         }
-        int maxKey = Collections.max(map.keySet());
-        System.out.println(map.get(maxKey) + " - number of letters " + maxKey);
+        int maxKey = 0; // Collections.max(map.keySet());
+//        System.out.println(map.get(maxKey) + " - number of letters " + maxKey);
+
+        maxKey = list1.indexOf(Collections.max(list1));
+        System.out.println(list.get(maxKey) + " - number of letters " + Collections.max(list1));
+
     }
 }
