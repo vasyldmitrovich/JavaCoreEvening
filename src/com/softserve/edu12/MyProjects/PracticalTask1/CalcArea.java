9e2da960aa9b5d76132cb3c0f23a30040a4ab057
Roman Sitko <sitkoroman@gmail.com>
2023-01-20 13:00:59 +0200

    Done. Lesson 12. Practical task 1

--- src/com/softserve/edu12/MyProjects/PracticalTask1/CalcArea.java
+++ src/com/softserve/edu12/MyProjects/PracticalTask1/CalcArea.java
@@ -7,22 +7,38 @@ import java.io.InputStreamReader;
 public class CalcArea {
 
     private int area;
+    int value;
 
     public CalcArea() {
         area = 0;
+        value = 0;
     }
 
     public int squareRectangle(int a, int b) throws IOException {
         if (a < 0 || b < 0) {
-            throw new IOException("negative!!!");
+            throw new IOException("Error. Some value is negative!!!");
         }
         area = a * b;
         return area;
     }
 
-    public int inputValues() throws IOException {
+    public int getArea() {
+        return area;
+    }
+
+    public int inputValues()  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int value = Integer.parseInt(br.readLine());
+        System.out.print("Enter value: ");
+        try {
+            value = Integer.parseInt(br.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Input error. Wrong value. Should be integer");
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("Input error. Some exception");
+            e.printStackTrace();
+            System.exit(0);
+        }
         return value;
     }
 }
--- src/com/softserve/edu12/MyProjects/PracticalTask1/Main.java
+++ src/com/softserve/edu12/MyProjects/PracticalTask1/Main.java
@@ -2,22 +2,25 @@ package com.softserve.edu12.MyProjects.PracticalTask1;
 
 import java.io.IOException;
 
-/* Romna Sitko
+/* Roman Sitko
 * Lesson 12. Practical task 1*/
+
 public class Main {
     public static void main(String[] args) {
-
+        run();
     }
 
-    public void run() {
+    public static void run() {
         int a = 0, b = 0;
         CalcArea calcArea = new CalcArea();
-        //a = calcArea.inputValues();
-        //b = calcArea.inputValues();
         try {
+            a = calcArea.inputValues();
+            b = calcArea.inputValues();
             calcArea.squareRectangle(a, b);
         } catch (IOException e) {
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
+        System.out.println("Area: " + calcArea.getArea());
     }
 }
