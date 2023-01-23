3e567cf3e5e7796f3d54bf91545c4eeaa71174c7
Oleg Lastochkin <oleglastochkin@gmail.com>
2023-01-23 20:32:24 +0200

    add task1

---
+++ src/com/softserve/Tasks/Task1.java
@@ -0,0 +1,21 @@
+package com.softserve.Tasks;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; ++i) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println("Fibonacci sequence");
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.println(arr[i]);
+        }
+    }
+}
