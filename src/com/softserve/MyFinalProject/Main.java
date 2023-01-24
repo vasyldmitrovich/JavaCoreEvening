fa5736bcf889bfefa588e53e1d868f21a775d93f
Roman Sitko <sitkoroman@gmail.com>
2023-01-24 23:12:22 +0200

    .

--- src/com/softserve/MyFinalProject/Main.java
+++ src/com/softserve/MyFinalProject/Main.java
@@ -1,26 +1,53 @@
 package com.softserve.MyFinalProject;
 
+import java.nio.charset.CharacterCodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         String str = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-        String str1 = "abc.as";
+        String str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String str1 = "Hello";
+
         char[] chStr = str.toCharArray();
         char[] chStr1 = str1.toCharArray();
+        char[] chStr2 = str2.toCharArray();
 
         StringBuilder sb = new StringBuilder();
-//        System.out.println(chStr[15]);
+        List<Character> list = new ArrayList<>();
 
-        for(int i = 0; i < chStr1.length; i++) {
-            for(int j = 0; j < 26; j++) {
-                if (chStr[j] == chStr[i]) {
-                    sb.append(chStr[j + 2]);
-                } else {
-                    sb.append(" ");
-                    j = 26;
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < chStr1.length; j++) {
+                if (chStr1[j] == chStr[i]) {
+                    list.add(chStr[i + 2]);
+                }
+                if (chStr1[j] == chStr2[i]) {
+                    list.add(chStr2[i + 2]);
                 }
             }
         }
 
+//        for(int i = 0; i < 26; i++) {
+//            for(int j = 0; j < chStr1.length; j++) {
+//                if ((chStr1[j] == chStr2[i]) && (Character.isUpperCase(chStr1[j]))) {
+//                    list.add(j, chStr2[i + 2]);
+//                }
+//            }
+//        }
+
+        for (int i = 0; i < chStr1.length; i++) {
+            if(!Character.isLetter(chStr1[i])){
+                list.add(i, chStr1[i]);
+            }
+        }
+
+
+
+
+        for(Character c : list) {
+            sb.append(c);
+        }
         System.out.println(sb);
     }
 }
