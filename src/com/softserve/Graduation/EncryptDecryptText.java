7f6224cde20b3576af01887fa345d0fca7b5c57a
Roman Sitko <sitkoroman@gmail.com>
2023-01-26 16:03:15 +0200

    Graduation. I think done

--- src/com/softserve/Graduation/EncryptDecryptText.java
+++ src/com/softserve/Graduation/EncryptDecryptText.java
@@ -8,6 +8,8 @@
 
 package com.softserve.Graduation;
 
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,6 +20,7 @@ public class EncryptDecryptText {
 
     private static final String EN_LOWER_ALPHABET = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
     private static final String EN_UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     private String inputStr;
     private byte key;
     private StringBuilder encText;
@@ -41,13 +44,13 @@ public class EncryptDecryptText {
     * Method for switching menu
     */
     public void startProgram () {
-        System.out.println("Start encrypt-decrypt program");
+        System.out.println("Start encrypt-decrypt program. Only ENGLISH text");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         byte tmpMenu = 0;
         boolean tmpBool = false;
         try {
             do {
-                System.out.print("Menu: 1-encrypt, 2-decrypt, 0-exit: ");
+                System.out.print("Menu: 1-encrypt , 2-decrypt, 0-exit: ");
                 tmpMenu = Byte.parseByte(br.readLine());
 
                 //Another way
@@ -55,17 +58,21 @@ public class EncryptDecryptText {
                     throw new IOException();
                 }
 */
+                /*
+                * Switching beetween items menu*/
                 switch (tmpMenu) {
                     case 1:
                         clearData();
-                        inputData((byte) 1);
-                        encryptText();
+                        if (inputData((byte) 1)) {
+                            encryptText();
+                        }
                         tmpBool = true;
                         break;
                     case 2:
                         clearData();
-                        inputData((byte) 2);
-                        decryptText();
+                        if (inputData((byte) 2)) {
+                            decryptText();
+                        }
                         tmpBool = true;
                         break;
                     case 0:
@@ -87,7 +94,7 @@ public class EncryptDecryptText {
     }
 
     /*
-     * Method for clearing data
+     * Method for clearing variables
      */
     private void clearData() {
         inputStr = "";
@@ -101,44 +108,50 @@ public class EncryptDecryptText {
     /*
      * Method for input text from console
      */
-    public void inputData(byte tmp) {
+    public boolean inputData(byte tmp) {
+        boolean returnVar = false;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             if(tmp == 1) {
                 System.out.print("Input text for encrypt: ");
                 inputStr = br.readLine();
-                if (inputStr.length() > 100) {
-                    throw new IOException();
+                if (inputStr.length() > 1000) {
+                    throw new IllegalArgumentException();
                 }
-                System.out.print("Input key for encrypt (0-26): ");
+                System.out.print("Input key for encrypt (0-25): ");
                 key = Byte.parseByte(br.readLine());
-                if ((key < 0) || (key > 26)) {
+                if ((key < 0) || (key > 25)) {
                     throw new IOException();
                 }
+                returnVar = true;
             }
             if (tmp == 2) {
                 System.out.print("Input text for decrypt: ");
                 inputStr = br.readLine();
                 if (inputStr.length() > 100) {
-                    throw new IOException();
+                    throw new IllegalArgumentException();
                 }
-                System.out.print("Input key for decrypt (0-26): ");
+                System.out.print("Input key for decrypt (0-25): ");
                 key = Byte.parseByte(br.readLine());
-                if ((key < 0) || (key > 26)) {
+                if ((key < 0) || (key > 25)) {
                     throw new IOException();
                 }
+                returnVar = true;
             }
         } catch (IOException e) {
-            System.err.println("\nKey should be between 0 and 26.");
+            returnVar = false;
+            System.err.println("\nKey should be between 0 and 25");
 //            e.printStackTrace();
         } catch (IllegalArgumentException e) {
-            System.err.println("\nText should be english alphabet (lower, upper case), digit and punctuation.");
+            returnVar = false;
+            System.err.println("\nText should be english alphabet (lower, upper case), digit and punctuation, max length 1000");
         } catch (Exception e) {
-            System.err.println("Error input data");
+            returnVar = false;
+            System.err.println("\nError input data");
             e.printStackTrace();
         }
+        return returnVar;
     }
-
 
     /*
      * Method for encrypt text
@@ -147,7 +160,7 @@ public class EncryptDecryptText {
         char[] charsOfLower_Alphabet = EN_LOWER_ALPHABET.toCharArray();
         char[] charsOfUpper_Alphabet = EN_UPPER_ALPHABET.toCharArray();
         char[] charsInputStr = inputStr.toCharArray();
-
+//        private static final String EN_LOWER_ALPHABET = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
         //Find letters(lower and upper case) and add new letters in accordance a key
         for(int i = 0; i < charsInputStr.length; i++) {
             for(int j = 0; j < 26; j++) {
@@ -185,7 +198,7 @@ public class EncryptDecryptText {
 
         //Find letters(lower and upper case) and add new letters in accordance a key
         for(int i = 0; i < charsInputStr.length; i++) {
-            for(int j = 0; j < 26; j++) {
+            for(int j = charsOfLower_Alphabet.length - 1; j > 27; j--) {
                 if (charsInputStr[i] == charsOfLower_Alphabet[j]) {
                     listDecText.add(charsOfLower_Alphabet[j - key]);
                 }
@@ -194,7 +207,7 @@ public class EncryptDecryptText {
                 }
             }
         }
-
+//        System.out.println("listDecText - " + listDecText.size() + listDecText);
         //Find non letter element and his position. Add this element to list
         for (int i = 0; i < charsInputStr.length; i++) {
             if(!Character.isLetter(charsInputStr[i])) {
