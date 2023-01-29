d00751d80c235a7c8f21f0acc5a2c2d906be4d45
YuriiShkimba <shkimba.yurii@gmail.com>
2023-01-29 19:33:51 +0200

    1

--- src/com/softserve/edu09/Main.java
+++ src/com/softserve/edu09/Main.java
@@ -2,7 +2,7 @@ package com.softserve.edu09;
 
 public class Main {
     public static void main(String[] args){
-        Collection c = new Collection(10, 25);
+        Collection c = new Collection(10, 45);
 
     }
 }
