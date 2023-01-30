b28b537f31e98f5c8a64f919201b4292aa8b8c78
Mykyta Stepanok <mykyta.stepanok@gmail.com>
2023-01-30 03:33:26 +0200

    edu 05 1st Practical tasks modified

--- src/com/softserve/edu05/practical/first/ArrayIntegers.java
+++ src/com/softserve/edu05/practical/first/ArrayIntegers.java
@@ -2,13 +2,7 @@ package practical.first;
 
 public class ArrayIntegers {
 
-    public int getMaxValue() {
-        return maxValue;
-    }
 
-    public static void setMaxValue(int value) {
-        maxValue = value;
-    }
 
     public int getSumPositive() {
         return sumPositive;
@@ -34,21 +28,24 @@ public class ArrayIntegers {
         ArrayIntegers.numberNegative = numberNegative;
     }
 
-    public static void CheckMaxValue(int value){
-        if(maxValue<value) setMaxValue(value);
+    public void CheckMaxValue(int value){
+
     }
 
 
 
 
 
-    static int maxValue,sumPositive,sumNegative,numberNegative=0,numberPositive=0;
+    static int sumPositive,sumNegative,numberNegative=0,numberPositive=0;
 
     public static void main(String[] args) {
 
+
         int[] arr= {10,41,59,7,992,1654,-127,11,25,0};
+        int maxValue= arr[0];
+
         for(int i : arr){
-            CheckMaxValue(i);
+            if(maxValue<i) maxValue=i;
             if(i>0){
                 sumPositive+=i;
                 numberPositive++;
