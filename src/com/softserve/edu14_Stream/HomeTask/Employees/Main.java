31fa42f76836a1d8f798797ff8ec3519b4754158
KaterinaLukyanenko <taka_78@i.ua>
2023-01-31 16:47:31 +0300

    add Task 2 in HomeTasks edu14

--- src/com/softserve/edu14_Stream/HomeTask/Employees/Main.java
+++ src/com/softserve/edu14_Stream/HomeTask/Employees/Main.java
@@ -1,13 +1,8 @@
 package com.softserve.edu14_Stream.HomeTask.Employees;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,23 +11,30 @@ public class Main {
 
     public static void go(){
         List<Employee> employees = createList();
+
+        Optional<String> name = mostPopularName(employees.stream());
+        if (name.isPresent()) {
+            System.out.println("Most popular name: " + name.get());
+        }
+        else {
+            System.out.println("Most popular name not found");
+        }
     }
 
-//    public static Optional<String> mostPopularName(Stream<Employee> employees){
-//
-////        public static <T> Set<T> findDuplicateByGrouping(List<T> list) {
-////
-////            return list.stream()
-////                    .collect(Collectors.groupingBy(Function.identity()
-////                            , Collectors.counting()))    // create a map {1=1, 2=1, 3=2, 4=2, 5=1, 7=1, 9=2}
-////                    .entrySet().stream()                 // Map -> Stream
-////                    .filter(m -> m.getValue() > 1)       // if map value > 1, duplicate element
-////                    .map(Map.Entry::getKey)
-////                    .collect(Collectors.toSet());
-////
-////        }
-//
-//    }
+    public static Optional<String> mostPopularName(Stream<Employee> employees){
+
+        Optional<Map.Entry<String,Long>> o = employees.collect(Collectors.groupingBy(Employee::getName,Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue());
+
+        Optional<String> name = Optional.empty();
+        if (o.isPresent()){
+                name = Optional.of(o.get()
+                .getKey());
+        }
+
+        return name;
+    }
 
     public static List<Employee> createList(){
         List<Employee> employees = new ArrayList<>();
@@ -42,6 +44,8 @@ public class Main {
         employees.add(new Employee("Bogdan"));
         employees.add(new Employee("Taras"));
         employees.add(new Employee("Maria"));
+        employees.add(new Employee("Bogdan"));
+        employees.add(new Employee("Taras"));
 
         return employees;
     }
