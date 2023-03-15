b8139c286992b2486ad5297cb266dc45a80fbeb5
Ruslan Zhyhunov <dngu@ukr.net>
2023-03-15 21:03:18 +0200

    12 task

--- src/com/softserve/edu12/homework/Main.java
+++ src/com/softserve/edu12/homework/Main.java
@@ -1,17 +1,29 @@
 package com.softserve.edu12.homework;
 
+import java.util.Arrays;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         try {
+            System.out.println("Enter two numbers to divide");
+
             div(scanner.nextDouble(), scanner.nextDouble());
         } catch (InputMismatchException e) {
             throw new InputMismatchException("Invalid character");
         } catch (ArithmeticException ex) {
             throw new ArithmeticException("Division by zero");
+        }
+
+
+        try {
+            realNumber(1, 100);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -21,6 +33,26 @@ public class Main {
         System.out.println(a / b);
     }
 
-/*    static void realNumber(int start, int and) {
-    }*/
+    static void realNumber(int start, int end) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int[] mas = new int[10];
+
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.print("Enter integer number: ");
+            int x = scanner.nextInt();
+            if (start <= x & x <= end) {
+                mas[i] = x;
+            } else {
+                throw new RuntimeException("The number is not in the given range");
+            }
+        }
+        Arrays.sort(mas);
+        for (int s : mas) {
+            System.out.print(s + ", ");
+        }
+
+    }
+
 }
